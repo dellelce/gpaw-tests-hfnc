c83d9fd6a986b6bdaa496e14a75e26d7a5359ac8
Antonio Dell'elce <antouk@dellelce.com>
2018-08-20 16:20:09 +0000

    Run 5

--- cycle.py
+++ cycle.py
@@ -8,7 +8,7 @@ from pprint import pprint
 from auto import execution
 
 
-def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05, atom=1, baselabel='xx'):
+def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05, atom=1, position=2, baselabel='xx'):
     with open(filename) as fh:
         b = fh.read()
         d = json.loads(b)
@@ -16,7 +16,7 @@ def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05, atom=1, basela
     status = start
 
     while status >= stop:
-        d['xyz'][atom][2] = status
+        d['xyz'][atom][position] = status
         pprint(d['xyz'][atom])
         label=str(status).replace('-','')
         label=label.replace('.','_')
@@ -33,5 +33,17 @@ if __name__ == '__main__':
 
     # run 2: cycle(start=-1.80, stop=-2.01, step=0.05, atom=1)
     # run 3 cycle(start=-1.825, stop=-2.01, step=0.05, atom=1)
-    # run 4:
-    cycle(start=-1.8125, stop=-2.01, step=0.05, atom=1)
+    # run 4: cycle(start=-1.8125, stop=-2.01, step=0.05, atom=1)
+
+    '''
+    up to run 4 the best energy is for HF-N: 1.8125 HF-C: 1.70 
+    from run 5 we start moving the carbon
+    '''
+    # run 5: cycle(start=-1.40, stop=-2.01, step=0.05, atom=2, position=1, baselabel='yy')
+
+    '''
+    run 5 moved carbon atom from 1.40 to 1.95, but 1.95 did not converge
+    Nitrogen stayed fixed at 1.45
+    Best energy for Hf-C at 1.90
+    '''
+    cycle(start=-1.40, stop=-2.01, step=0.05, atom=2, position=1, baselabel='yy')
