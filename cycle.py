54a530f27660cb6c45427f3f5bd946281dac3913
Antonio Dell'elce <antouk@dellelce.com>
2018-08-20 15:36:33 +0000

    cycle.py: run 2

--- cycle.py
+++ cycle.py
@@ -8,7 +8,7 @@ from pprint import pprint
 from auto import execution
 
 
-def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05):
+def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05, atom=1, baselabel='xx'):
     with open(filename) as fh:
         b = fh.read()
         d = json.loads(b)
@@ -16,16 +16,19 @@ def cycle(filename="19.json", start=-1.10, stop=-1.80, step=0.05):
     status = start
 
     while status >= stop:
-        d['xyz'][1][2] = status
-        pprint(d['xyz'][1])
+        d['xyz'][atom][2] = status
+        pprint(d['xyz'][atom])
         label=str(status).replace('-','')
         label=label.replace('.','_')
+        label=label[:6]
         x = execution(d)
-        x.run('xx'+label)
+        x.run(baselabel+label)
         status -= step
 
 
 
 
 if __name__ == '__main__':
-    cycle()
+    # initial cycle: start=-1.10, stop=-1.80, step=0.05, atom=1
+
+    cycle(start=-1.80, stop=-2.01, step=0.05, atom=1)
